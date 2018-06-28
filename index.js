01fe0630778ad1f304f0461ffba103e14328a91a
Brennan B <bdbasinger777@gmail.com>
2018-06-28 15:25:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 
 function destructivelyAppendKitten(array, kitten) {
-  return array.push(kitten)
+  array.push(kitten)
+  return array
 }
